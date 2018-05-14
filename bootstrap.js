0e794edd83386fc084a40eac571dd5e37b67f7e7
Erik Landvall <erik@landvall.se>
2018-05-14 13:28:57 +0200

    Cleaned up the bootstrap process

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 const
-handlebars = require('handlebars'),
-config = require('./config'),
-log = require('@superhero/debug').log
+handlebars  = require('handlebars'),
+config      = require('./config'),
+log         = require('@superhero/debug').log
 
 addHelper(name, filename)
 {
@@ -36,46 +36,46 @@ module.exports = (options, config_core) =>
 
   log(`Added the view "handlebars" to the list of accepted view servants`)
 
-  if('path' in options.handlebars)
+  if('path' in options)
   {
-    log(`Set the public directory to: ${options.handlebars.path}"`)
-    config.path = options.handlebars.path
+    log(`Set the public directory to: ${options.path}"`)
+    config.path = options.path
   }
 
-  if('partials' in options.handlebars)
+  if('partials' in options)
   {
     log(`Adding partials`)
-    for(let partial in options.handlebars.partials)
+    for(let partial in options.partials)
     {
       log(`Adding partial:"${partial}"`)
 
       const
-      template = options.handlebars.partials[partial],
+      template = options.partials[partial],
       source = fs.readFileSync(`${config.path}/${template}.hbs`, 'utf-8')
 
       handlebars.registerPartial(name, source)
     }
   }
 
-  if('helpers' in options.handlebars)
+  if('helpers' in options)
   {
     log(`Adding helpers`)
-    if(Array.isArray(options.handlebars.helpers))
+    if(Array.isArray(options.helpers))
     {
       log(`Adding helpers by Array`)
-      for(const name of options.handlebars.helpers)
+      for(const name of options.helpers)
       {
         log(`Adding helper:"${name}"`)
         addHelper(name)
       }
     }
 
-    else if(typeof options.handlebars.helpers === 'object')
+    else if(typeof options.helpers === 'object')
     {
       log(`Adding helpers by object`)
-      for(const name in options.handlebars.helpers)
+      for(const name in options.helpers)
       {
-        const filepath = options.handlebars.helpers[name]
+        const filepath = options.helpers[name]
 
         // if true, then assume attempting to add a library helper
         if(filepath === true)
@@ -98,7 +98,7 @@ module.exports = (options, config_core) =>
     else
     {
       const
-      type  = typeof options.handlebars.helpers,
+      type  = typeof options.helpers,
       msg   = `Invalid format for "config.handlebars.helpers". `
             + `Expected "object" or "Array", found:"${type}"`
       error = new TypeError(msg)
